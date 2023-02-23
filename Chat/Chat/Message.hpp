656f4678abe1e7756b454c72d2514df864990b96
ndrjjzbv <ndrjjzbv@rambler.ru>
2023-02-23 12:21:05 +0300

    Declare classes Message

--- Chat/Chat/Message.hpp
+++ Chat/Chat/Message.hpp
@@ -1,1 +1,14 @@
 #pragma once
+#include<string>
+class Message
+{
+public:
+    Message(const std::string& from, const std::string& to, const std::string& text);
+    const std::string& getFrom() const noexcept;
+    const std::string& getTo() const noexcept;
+    const std::string& getText() const noexcept;
+private:
+    std::string _from;
+    std::string _to;
+    std::string _text;
+};
