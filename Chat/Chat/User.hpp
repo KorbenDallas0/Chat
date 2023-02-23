962d02d4c840ddd4670a547279b31086303f4d82
ndrjjzbv <ndrjjzbv@rambler.ru>
2023-02-23 12:17:09 +0300

    Declare class User

--- Chat/Chat/User.hpp
+++ Chat/Chat/User.hpp
@@ -1,1 +1,20 @@
 #pragma once
+#include<string>
+
+class User final {
+public:
+    User(const std::string& login, const std::string& password, const std::string& name);
+
+    const std::string getLogin() const noexcept;
+
+    void setPassword(const std::string& pass) noexcept;
+    const std::string& getPassword() const noexcept;
+
+    void setName(const std::string& name) noexcept;
+    const std::string& getName() const noexcept;
+
+private:
+    std::string _login;
+    std::string _password;
+    std::string _name;
+};
