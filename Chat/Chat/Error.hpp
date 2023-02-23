941c563c09a4501322fa7454a537d8be3c2a095e
ndrjjzbv <ndrjjzbv@rambler.ru>
2023-02-23 12:09:33 +0300

    Declare classes Login_busy, Incorrect_Login_Or_Password, LoginNotFound

--- Chat/Chat/Error.hpp
+++ Chat/Chat/Error.hpp
@@ -1,1 +1,17 @@
 #pragma once
+#include<exception>
+
+class Login_busy final : public std::exception {
+public:
+    const char* what() const noexcept override;
+};
+
+class Incorrect_Login_Or_Password final : public std::exception {
+public:
+    const char* what() const noexcept override;
+};
+
+class LoginNotFound final : public std::exception {
+public:
+    const char* what() const noexcept override;
+};
